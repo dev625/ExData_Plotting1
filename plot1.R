6546247f20179ffc134496293e05984d30be4a83
Devesh Lohumi <witdx514@gmail.com>
2021-04-01 20:44:02 +0530

    First Commit

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# Author : Devesh Lohumi
+# To Plot : Histogram of Global Active Power
+
+
+df <- read.delim("../household_power_consumption.txt", header=T, sep=';', na.strings="?")
+df$Date <- as.Date(df$Date, format="%d/%m/%Y")#date is formatted for ease of getting a subset of data
+
+df <- subset(df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))#getting the subset of data
+
+hist(df$Global_active_power,main="Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab="Frequency",col="red")
+
+dev.copy(png, file="plot1.png", height=480, width=480) #copying the plot to a png file
+dev.off()
