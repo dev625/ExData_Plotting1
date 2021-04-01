ef94151a15d56247899a9730d65eafe079b9df57
Devesh Lohumi <witdx514@gmail.com>
2021-04-01 22:19:12 +0530

    added units argument and plot2

--- plot1.R
+++ plot1.R
@@ -11,5 +11,5 @@ hist(df$Global_active_power,main="Global Active Power",
      xlab = "Global Active Power (kilowatts)",
      ylab="Frequency",col="red")
 
-dev.copy(png, file="plot1.png", height=480, width=480) #copying the plot to a png file
+dev.copy(png, file="plot1.png", height=480, width=480,units="px") #copying the plot to a png file
 dev.off()
---
+++ plot2.R
@@ -0,0 +1,17 @@
+# Author : Devesh Lohumi
+# To Plot : Global Active Power vs Days
+
+
+df <- read.delim("../household_power_consumption.txt", header=T, sep=';', na.strings="?")
+df$Date <- as.Date(df$Date, format="%d/%m/%Y")#date is formatted for ease of getting a subset of data
+
+df <- subset(df, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))#getting the subset of data
+
+Date_time <- paste(as.Date(df$Date), df$Time)
+df$Date_time <- as.POSIXct(Date_time)
+
+png(filename="plot2.png",height=480,width=480,units = "px")
+
+plot(df$Date_time,df$Global_active_power,type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+axis(1, at = c(1, 1200, 1400), labels = c("Thu", "Fri", "Sat"))
+dev.off()
